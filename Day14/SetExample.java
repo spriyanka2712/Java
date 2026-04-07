70e663d54ad593d497b82e3b931dfd2f2694eec2
spriyanka2712 <spriyanka27122005@gmail.com>
2026-04-07 14:22:34 +0530

    commit

---
+++ Day14/SetExample.java
@@ -0,0 +1,15 @@
+package Day14;
+import java.util.*;
+public class SetExample {
+    public static void main(String[] args){
+        Set<Integer> Linkedset = new LinkedHashSet<>();
+        Set<Integer> Hset = new HashSet<>();
+        int arr[] = {5, 3, 9, 1, 7, 2};
+        for(int i : arr){
+            Linkedset.add(i);
+            Hset.add(i);
+        }
+        System.out.println(Linkedset);
+        System.out.println(Hset);
+    }
+}
